c44a08342222d094a5326d858d8701dbec93bdfc
Krystian98 <85512163+Krystian98@users.noreply.github.com>
2021-06-11 01:31:35 +0200

    Draft 2

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 import "./footer.css";
 
 export default function Footer() {
     return (
-      <div className="home-container">  
-      HEEEI
+      <div className="home-container">
+          <SocialIcon url="mailto:kystiankrawczyk98@gmail.com" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35}}/>
+          <SocialIcon url="https://www.instagram.com/krystianbp/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35}}/>    
       </div>
     );
   }
