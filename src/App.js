e59ff99e2965096b8a56139a681f1c8d03e74992
Thorarinn S Gunnarsson <torarinn91@gmail.com>
2021-06-10 20:58:22 +0000

    fixing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./portfolio/home";
-import About from "./portfolio/about"
-import Work from "./portfolio/work";
-import Navbar from "./portfolio/navbar";
+import Home from "./components/home";
+import About from "./components/about";
+import Work from "./components/work";
+import Navbar from "./components/navbar";
 
 function App() {
-  return ( 
+  return (
     <BrowserRouter>
-    <Navbar />
-     <Switch>
-       <Route component={Home} path='/' exact/>
-       <Route component={About} path='/about' />
-       <Route component={Work} path='/work' />
-     </Switch>
+      <Navbar />
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={Work} path="/work" />
+      </Switch>
     </BrowserRouter>
- )
+  );
 }
 
 export default App;
