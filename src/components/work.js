5a74a4126389d522763cfe80d66f11ec36a48f9f
Thorarinn S Gunnarsson <torarinn91@gmail.com>
2021-06-11 17:59:32 +0000

    hover over picture displays text in the corner of the picture among hovering over the title that pops up brings up more detailed description of a specific project

--- src/components/work.js
+++ src/components/work.js
@@ -4,18 +4,28 @@ import { Link } from "react-router-dom";
 import "./work.css";
 import data from "../static/data/projects";
 
+import complex_building from "../static/complex-building.jpeg";
+
 export default function Work() {
   const proj = data.projects;
   return (
-    <div className="work-container">
+    <div className="work-main-container">
       {proj.map((p) => {
         return (
-          <div className="work-proj">
-            <Link to={{ pathname: `/project/${p.text}`, state: p }} exact>
+          <Link
+            className="work-proj"
+            to={{ pathname: `/project/${p.text}`, state: p }}
+            exact
+          >
+            <div className="work-container">
               <img className="work-pic" src={p.img} />
-              <div className="work-text">{p.text}</div>
-            </Link>
-          </div>
+              <div className="work-text work-absolute">{p.text}</div>
+              <div className="work-detailed-text work-absolute">
+                {" "}
+                {p.detailed_text}{" "}
+              </div>
+            </div>
+          </Link>
         );
       })}
     </div>
--- src/static/data/projects.js
+++ src/static/data/projects.js
@@ -8,12 +8,22 @@ export default {
       id: 1,
       img: bofelleskap_pic,
       text: "Bofelleskap",
-      detailed_text: `Espen Fin­stad, forn­leifa­fræðing­ur hjá Inn­land­et-fylki í Nor­egi, vinn­ur nú að rann­sókn­um og upp­greftri tófta allt að tutt­ugu húsa í Bøver-daln­um í Lom, milli Breheimen og Jot­un­heimen. Virðist þar vera um heilt þorp að ræða og hafa nokk­ur hús­anna verið lang­hús, það stærsta um 100 fer­metr­ar, og benda ald­urs­grein­ing­ar kola úr eld­stæðum til ára­bils­ins 700 til 1100, vík­inga­tíma­bils­ins svo­nefnda.
-        Mik­il­vægt er að leiða í ljós hvort hér hafi verið búið allt árið,“ út­skýr­ir Fin­stad fyr­ir norska rík­is­út­varp­inu NRK, en sá sem fann hluta þess­ara gömlu bú­staða á sín­um tíma er Rei­dar Marstein, áhuga­forn­leifa­fræðing­ur sem þekk­ir hverja þúfu á svæðinu. Það var árið 2011, en það er fyrst nú sem hægt er að hefja rann­sókn­ir á þeim vegna þess hve mikið jökl­ar í dals­hlíðunum hafa hopað síðustu ár.`,
+      detailed_text: `Espen Fin­stad, forn­leifa­fræðing­ur hjá Inn­land­et-fylki í Nor­egi, vinn­ur nú að rann­sókn­um og upp­greftri tófta allt að tutt­ugu húsa í Bøver-daln­um í Lom, milli Breheimen og Jot­un­heimen. Virðist þar vera um heilt þorp að ræða og hafa nokk­ur hús­anna verið lang­hús, það stærsta um 100 fer­metr­ar, og benda ald­urs­grein­ing­ar kola úr eld­stæðum til ára­bils­ins 700 til 1100, vík­inga­tíma­bils­ins svo­nefnda.`,
       moreImgs: [1, 2, 3],
     },
-    { id: 2, img: complex_building, text: "Complex" },
-    { id: 3, img: hammock, text: "Coomplex2" },
+    {
+      id: 2,
+      img: complex_building,
+      text: "Complex",
+      detailed_text: `Á tólfta tím­an­um í dag klöngraðist mann­eskja upp á eld­fjallið í Geld­inga­döl­um og átti svo fót­um sín­um fjör að launa þegar hraun kom flæðandi niður hlíðina. Hægt var að sjá í mynda­vél mbl.is í Geld­inga­döl­um rétt rúm­lega ell­efu þegar mann­eskja hljóp und­an flæðandi hraun­inu.
+    Mynd­skeið af at­vik­inu má sjá hér að ofan. `,
+    },
+    {
+      id: 3,
+      img: hammock,
+      text: "Coomplex2",
+      detailed_text: `„Það er ákveðið vanda­mál hversu stór hluti þjóðar­inn­ar teng­ir ekki við sjáv­ar­út­veg. Það bara hinn póli­tíski veru­leiki, eða sam­fé­lags­legri veru­leiki,“ sagði Þór­dís Kol­brún Reyk­fjörð Gylfa­dótt­ir, ferðamála-, iðnaðar- og ný­sköp­un­ar­ráðherra á mál­stof­unni Far­sælt sam­band sjáv­ar­út­vegs og ís­lensks hug­vits - Auðlind vex af auðlind. `,
+    },
     {
       id: 1,
       img: bofelleskap_pic,
